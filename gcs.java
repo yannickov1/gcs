dc37f122e0e1e672385858bb4f56221f2bff5a7a
Yannick <yannickov@gmail.com>
2024-03-10 16:50:09 -0500

    cambio de prueba en ramas

--- gcs.java
+++ gcs.java
@@ -2,5 +2,7 @@ public class gcs {
     public static void main(String[] args) {
         System.out.println("no hay cambios");
         System.out.println("primer cambio");
+        System.out.println("cambio en rama");
+        System.out.println("otro cambio");
     }
 }
